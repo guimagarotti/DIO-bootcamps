434735be31616e2a90d44574fb9706c1a2466861
Guilherme Magarotti <guimagarotti@hotmail.com>
2022-06-24 09:19:15 -0300

    curso-javascript

---
+++ curso-javascript/modulos/assets/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("Guilherme", 18));
+console.log(mostraCidade("Guilherme", "Sorocaba"));
+console.log(mostraHobby("Guilherme", "jogar Futebol"));
